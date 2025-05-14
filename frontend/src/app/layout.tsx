0304852e7cff2dc5a205251265d71e1a6e6e8ea7
gonzalopozo <gonzalopozo2005@gmail.com>
2025-05-14 15:43:51 +0200

    fix: modo oscuro arreglado y tema definido por defecto del sistema

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type React from "react"
+import type { ReactNode } from "react"
 import "./globals.css"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -13,12 +13,12 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: ReactNode
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>
