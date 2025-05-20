47d1636213c510599c4e254acb4b807e717ba28e
gonzalopozo <gonzalopozo2005@gmail.com>
2025-05-20 12:24:36 +0200

    feat: logo no obligatorio en la interfaz de los equipos para posteriormente añadirlo en base al nombre del equipo

--- frontend/src/types/index.d.ts
+++ frontend/src/types/index.d.ts
@@ -62,7 +62,7 @@ export interface TokenResponse {
 export interface Team {
     id: string
     name: string
-    logo: LucideIcon
+    logo?: LucideIcon
     // conference: string
     // division: string
     record: {
